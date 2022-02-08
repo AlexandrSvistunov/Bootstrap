796683796b3d0d9161647e22b7da21e9d5e077d3
Alexandr Svistunov <alsvis2019@gmail.com>
2022-02-08 18:28:57 +0500

    Фильтр галереи оптимизация

--- js/main.js
+++ js/main.js
@@ -2,58 +2,31 @@ $(document).ready(function() {
 
     // ****** Фильтр галереи******
 
-    $('.button[filter="web"]').click(function() {
+    $('.button[filter]').click(function() {
+        if ($(this).attr('filter') == 'all') {
+            if ($(this).attr('val') == 'off') {
+                $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
+
+                $(this).attr('val', 'on'); //Присваиваем у нажатой кнопки атрибуту "val" значение "on" 
+
+                $('.filter > div').show(300); // Показываем все элементы
+
+            }
+
+        } else
+
         if ($(this).attr('val') == 'off') {
             $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
 
-            $(this).attr('val', 'on'); //Присваиваем у кнопки "web" атрибуту "val" значение "on" 
+            $(this).attr('val', 'on'); //Присваиваем у нажатой кнопки атрибуту "val" значение "on" 
 
             $('.filter > div').hide(300); // Скрываем все элементы
 
-            $('.filter > div[filter="web"]').show(300); // Показываем нужные элементы
+            var filter = $(this).attr('filter');
+
+            $('.filter > div[filter=' + filter + ']').show(300); // Показываем нужные элементы
         }
+
     });
-
-    // То же самое для кнопки design
-
-    $('.button[filter="design"]').click(function() {
-        if ($(this).attr('val') == 'off') {
-            $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
-
-            $(this).attr('val', 'on'); //Присваиваем у кнопки "design" атрибуту "val" значение "on" 
-
-            $('.filter > div').hide(300); // Скрываем все элементы
-
-            $('.filter > div[filter="design"]').show(300); // Показываем нужные элементы
-        }
-    });
-
-    // То же самое для кнопки mockups
-
-    $('.button[filter="mockups"]').click(function() {
-        if ($(this).attr('val') == 'off') {
-            $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
-
-            $(this).attr('val', 'on'); //Присваиваем у кнопки "mockups" атрибуту "val" значение "on" 
-
-            $('.filter > div').hide(300); // Скрываем все элементы
-
-            $('.filter > div[filter="mockups"]').show(300); // Показываем нужные элементы
-        }
-    });
-
-    // Для кнопки all
-
-    $('.button[filter="all"]').click(function() {
-        if ($(this).attr('val') == 'off') {
-            $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
-
-            $(this).attr('val', 'on'); //Присваиваем у кнопки "all" атрибуту "val" значение "on" 
-
-            $('.filter > div').show(300); // Показываем все элементы
-        }
-    });
-
-
 
 });
