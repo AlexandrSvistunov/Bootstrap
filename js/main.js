639bc8baaa946fa52ac2dc14c82870098a5391fc
Alexandr Svistunov <alsvis2019@gmail.com>
2022-02-09 17:37:38 +0500

    Оптимизация скрипта фильтра галереи

--- js/main.js
+++ js/main.js
@@ -3,17 +3,6 @@ $(document).ready(function() {
     // ****** Фильтр галереи******
 
     $('.button[filter]').click(function() {
-        if ($(this).attr('filter') == 'all') {
-            if ($(this).attr('val') == 'off') {
-                $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
-
-                $(this).attr('val', 'on'); //Присваиваем у нажатой кнопки атрибуту "val" значение "on" 
-
-                $('.filter > div').show(300); // Показываем все элементы
-
-            }
-
-        } else
 
         if ($(this).attr('val') == 'off') {
             $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
@@ -22,9 +11,18 @@ $(document).ready(function() {
 
             $('.filter > div').hide(300); // Скрываем все элементы
 
-            var filter = $(this).attr('filter');
+            var filter = $(this).attr('filter'); // Присваиваем переменной filter значение атрибута filter нажатой кнопки
 
-            $('.filter > div[filter=' + filter + ']').show(300); // Показываем нужные элементы
+            $('.filter > div[filter=' + filter + ']').show(300); // Показываем элементы с нужным атрибутом
+
+            if ($(this).attr('filter') == 'all') {
+                $('.button[filter]').attr('val', 'off'); // Присваиваем у всех кнопок атрибуту "val" значение "off" 
+
+                $(this).attr('val', 'on'); //Присваиваем у нажатой кнопки атрибуту "val" значение "on" 
+
+                $('.filter > div').show(300); // Показываем все элементы
+            }
+
         }
 
     });
